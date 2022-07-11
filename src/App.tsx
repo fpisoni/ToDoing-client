5754d42277174c10673d6860f8d41e208cc745dd
Felipe Pisoni <felipe.pisoni@globallogic.com>
2022-07-11 16:03:04 -0300

    starting changes, pending corrections

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Task from './components/task';
+import { TaskModel } from './models/task.model';
+import TaskCreate from './pages/tasks/taskCreate';
+import Tasks from './pages/tasks/tasks';
+import TaskService from './utils/taskService';
 
 function App() {
+
+  const {getTasks,editTask,createTask} = TaskService()
+
+  const taskArray: TaskModel[] = [];
+
+  const [tasks, setTasks] = useState([])
+  getTasks().then(tasks => setTasks(tasks))
+
+  const creationHandler = (title: string) => setTasks([ ...tasks, {id: tasks[tasks.length-1].id+1, title:title} ]);
+  const deleteHandler = (id: number) => console.log(tasks.find(task => task.id === id));
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div className='App-body'>
+        <Tasks tasks={tasks} onDelete={deleteHandler}/>
+        <TaskCreate creationHandler={creationHandler} />
+      </div>
     </div>
   );
 }
